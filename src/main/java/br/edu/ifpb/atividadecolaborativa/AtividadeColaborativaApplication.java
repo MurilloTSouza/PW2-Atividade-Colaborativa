d974a978c99935378b65070a31ee960719be3c82
Murillo <murillo_eum@hotmail.com>
2020-12-09 16:05:10 -0300

    initializer

---
+++ src/main/java/br/edu/ifpb/atividadecolaborativa/AtividadeColaborativaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.atividadecolaborativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeColaborativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeColaborativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/atividadecolaborativa/AtividadeColaborativaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.atividadecolaborativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeColaborativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
